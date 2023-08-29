463bd096e6ac1e21addec5d088a6ec837dfe55db
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-08-29 09:20:46 +0200

    Ex5 test1

---
+++ ExerciseLibrary/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseLibrary
+{
+    public class Product
+    {
+        private string type;
+        private string name;
+        private int sellIn;
+        private int quality;
+    }
+}
